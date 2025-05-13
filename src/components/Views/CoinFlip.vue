5e64b153f03135dafe70f7f8f8fd44fa7237e5d2
N-eeraj <neeraj7rajeena@gmail.com>
2025-05-13 20:11:37 +0530

    add images for die & coin faces

--- src/components/Views/CoinFlip.vue
+++ src/components/Views/CoinFlip.vue
@@ -7,16 +7,32 @@ import { randomBoolean } from "@n-eeraj/random"
 
 import Home from "../Home.vue"
 import Button from "../Base/Button.vue"
-import useTriggerAction from "../../composables/useTriggerAction"
+import useAppContext from "../../composables/useAppContext"
+
+const { app } = useAppContext()
 
 const coinFace = ref<boolean | null>(null)
-const isInitialTrigger = computed(() => coinFace.value === null)
+const isAnimating = ref(false)
+const loading = ref(false)
+const sfx = new Audio("/sfx/coin.mp3")
 
-const {
-  loading,
-  isAnimating,
-  triggerAction,
-} = useTriggerAction(() =>  coinFace.value = randomBoolean(), isInitialTrigger, "/sfx/coin.mp3")
+const coinFaceText = computed(() => coinFace.value ? "head" : "tail")
+
+function triggerAction() {
+  loading.value = true
+  isAnimating.value = true
+  const animationTime = coinFace.value === null ? 1000 : 2000
+  if (app.sound) {
+    setTimeout(() => {
+      sfx.play()
+    }, animationTime - 1000)
+  }
+  setTimeout(() => {
+    coinFace.value = randomBoolean()
+    isAnimating.value = false
+    setTimeout(() =>  loading.value = false, animationTime)
+  }, animationTime)
+}
 </script>
 
 <template>
@@ -26,16 +42,24 @@ const {
     </h1>
     <Home />
 
-    <div class="flex-1 overflow-y-hidden">
+    <div class="grid place-content-center flex-1 overflow-y-hidden">
       <Transition mode="out-in">
         <img
           v-if="isAnimating"
           src="/animations/coin-flip.gif"
           alt="coin flip animation"
-          class="w-20 m-auto -translate-y-full" />
-        <span v-else-if="coinFace !== null">
-          {{ coinFace ? "Head" : "Tail" }}
-        </span>
+          class="w-20 -translate-y-full" />
+        <div
+          v-else-if="coinFace !== null"
+          class="flex flex-col items-center gap-y-1">
+          <img
+            :src="`/images/coin-face/${coinFaceText}.webp`"
+            :alt="coinFaceText"
+            class="w-12 m-auto" />
+          <span class="capitalize">
+            {{ coinFaceText }}
+          </span>
+        </div>
       </Transition>
     </div>
 
--- src/components/Views/DiceRoll.vue
+++ src/components/Views/DiceRoll.vue
@@ -7,21 +7,30 @@ import RandomNumber from "@n-eeraj/random/RandomNumber"
 
 import Home from "../Home.vue"
 import Button from "../Base/Button.vue"
-import useTriggerAction from "../../composables/useTriggerAction"
+import useAppContext from "../../composables/useAppContext"
+
+const { app } = useAppContext()
 
 const diceFace = ref<number | null>(null)
-  const isInitialTrigger = computed(() => diceFace.value === null)
+const isAnimating = ref(false)
+const loading = ref(false)
+const sfx = new Audio("/sfx/dice.mp3")
 
-const {
-  loading,
-  isAnimating,
-  triggerAction,
-} = useTriggerAction(() => {
-  diceFace.value = RandomNumber.int({
-    min: 1,
-    max: 6,
-  })
-}, isInitialTrigger, "/sfx/dice.mp3")
+function triggerAction() {
+  loading.value = true
+  isAnimating.value = true
+  if (app.sound) {
+    sfx.play()
+  }
+  setTimeout(() => {
+    diceFace.value = RandomNumber.int({
+      min: 1,
+      max: 6,
+    })
+    isAnimating.value = false
+    setTimeout(() =>  loading.value = false, 1000)
+  }, 1000)
+}
 </script>
 
 <template>
@@ -31,16 +40,20 @@ const {
     </h1>
     <Home />
 
-    <div class="flex-1 overflow-x-hidden">
-      <Transition mode="out-in">
+    <div class="relative grid place-content-center flex-1 overflow-y-hidden">
+      <Transition name="animation">
         <img
           v-if="isAnimating"
           src="/animations/die-roll.gif"
           alt="dice roll animation"
           class="w-20 m-auto" />
-        <span v-else-if="diceFace !== null">
-          {{ diceFace }}
-        </span>
+      </Transition>
+      <Transition name="view">
+        <img
+          v-if="!loading && diceFace !== null"
+          :src="`/images/dice/${diceFace}.svg`"
+          :alt="`dice-face-${diceFace}`"
+          class="size-16" />
       </Transition>
     </div>
 
@@ -54,15 +67,26 @@ const {
 </template>
 
 <style scoped>
-.v-enter-active,
-.v-leave-active {
+.animation-enter-active,
+.animation-leave-active {
   @apply ease-linear duration-1000;
 }
 
-.v-enter-from {
+.animation-enter-from {
   @apply -translate-x-[200%];
 }
-.v-leave-to {
+.animation-leave-to {
   @apply translate-x-[200%];
+}
+
+.view-enter-active {
+  @apply transition-opacity duration-1000;
+}
+.view-leave-active {
+  @apply transition-opacity duration-0;
+}
+
+.view-enter-from, .view-leave-to {
+  @apply opacity-0;
 }
 </style>
